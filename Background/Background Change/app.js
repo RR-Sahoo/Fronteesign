496ef69a6bb5b5165d012b8453d845f0f3dbba88
Anwesh Mishra <anweshm3@gmail.com>
2021-09-30 21:03:57 +0530

    Background Change

---
+++ Background/Background Change/app.js
@@ -0,0 +1,19 @@
+var win = $(window);
+
+var allMods = $("block-reveal");
+
+allMods.each(function (i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function (event) {
+  allMods.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("block-reveal-inner");
+    }
+  });
+});
